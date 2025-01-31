43240bbcf31dced03bfe1b3402e301a1466cb56d
Hariom-vishwa <hawkgamerhv@gmail.com>
2025-01-31 11:41:20 +0530

    Script changes

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ closePopUp.addEventListener("click", () => {
 });
 
 addNote.addEventListener("click", () => {
+if(titleInp.value=="" || contentInp.value==""){
+  alert("Fill all the required fields.")
+}
+else{
   let note = document.createElement("div");
   note.classList.add("notes");
   note.innerHTML = `
                     <div class="actions">
-                        <button><i class="ri-edit-line"></i></button>
-                        <button><i class="ri-delete-bin-4-line"></i></button>
+                        <button id="noteEdit"><i class="ri-edit-line"></i></button>
+                        <button id="noteDelete"><i class="ri-delete-bin-4-line"></i></button>
                     </div>
                     <h3>${titleInp.value}</h3>
                     <div class="dateTime"></div>
@@ -28,5 +32,14 @@ addNote.addEventListener("click", () => {
 
   document.querySelector(".notesCont").appendChild(note);
   newNote.style.display = "none";
+  titleInp.value="";
+  contentInp.value=""
+}
 });
+
+document.getElementById("noteDelete").addEventListener("click", (e)=>{
+if(e.target.tagName == "button"){
+  e.target.parentElement.remove()
+}
+})
 
